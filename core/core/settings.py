8bb66814cdaeb28cce55c13b7069e63f42acecf6
Mohammad222PR <mohammades13851@gmail.com>
2023-10-21 02:07:13 +0330

    commit

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # Install App.
+    'accounts.apps.AccountsConfig',
+    'blog.apps.BlogConfig',
+    # Install Package.
 ]
 
 MIDDLEWARE = [
