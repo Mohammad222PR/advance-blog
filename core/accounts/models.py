2c8fb26abe99136c813772cdebeedd095828ca7c
Mohammad222PR <mohammades13851@gmail.com>
2023-10-21 21:41:16 +0330

    commit 123

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+    )
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extera_fields):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+        email=self.normalize_email(email)
+        user = self.model(email=email, **extera_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extera_fields):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+
+        extera_fields.setdefault('is_staff',True)
+        extera_fields.setdefault('is_superuser',True)
+        extera_fields.setdefault('is_active',True)
+
+        if extera_fields.get('is_staff') is not True:
+            raise ValueError(_("staff user most be not True"))
+        
+        if extera_fields.get('is_superuser') is not True:
+            raise ValueError(_("super user most be not True"))
+        self.create_user(email, password, **extera_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=10000)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='sub')
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title 
+    
+class Tag(models.Model):
+    title = models.CharField(max_length=10000)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title 
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    title = models.CharField(max_length=10000)
+    image = models.ImageField(upload_to='images/posts', null=True, blank=True)
+    content = models.TextField(max_length=1000000000)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+    # Time Field. 
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_day = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title 
