7fb30c1591942affdecefb5c166781fed96e512e
Mohammad222PR <mohammades13851@gmail.com>
2023-10-25 19:54:17 +0330

    new

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import *
+from django.views.generic.base import TemplateView
 # Create your views here.
 
 class indexView(View):
@@ -12,3 +13,12 @@ class IndexPostDetail(View):
     def get(self, request, pk):
         post = Post.objects.get(id=pk)
         return render(request, 'blog/detail.html', {'post':post})
+    
+# class IndexView(TemplateView):
+#     template_name = 'blog/index.html'
+
+#     def get_context_data(self, **kwargs):
+#         # Call the base implementation first to get a context
+#         context = super().get_context_data(**kwargs)
+#         context['post'] = Post.objects.all()
+#         return context
