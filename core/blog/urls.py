4de46180eb23620aa7b64fb263105e85f668bf15
Mohammad222PR <mohammades13851@gmail.com>
2023-10-27 00:59:53 +0330

    COMMIT

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.indexView.as_view(), name='blog'),
+    path('', views.PostView.as_view(), name='blog'),
     path('detail/<int:pk>', views.IndexPostDetail.as_view(), name='blog_detail'),
-    path('redirect', views.IndexRedirect.as_view(), name='redirect'),
+    # path('redirect', views.IndexRedirect.as_view(), name='redirect'),
 
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -3,30 +3,43 @@ from django.shortcuts import render
 from django.views import View
 from .models import *
 from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView
 # Create your views here.
 
-class indexView(View):
-    def get(self, request):
-        post = Post.objects.all()
-        return render(request, 'blog/index.html', {'post':post})
+# class PostView(View):
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         return render(request, 'blog/index.html', {'posts':posts})
 
 class IndexPostDetail(View):
     def get(self, request, pk):
         post = Post.objects.get(id=pk)
         return render(request, 'blog/detail.html', {'post':post})
     
-# class IndexView(TemplateView):
-#     template_name = 'blog/index.html'
+# # class IndexView(TemplateView):
+# #     template_name = 'blog/index.html'
 
-#     def get_context_data(self, **kwargs):
-#         # Call the base implementation first to get a context
-#         context = super().get_context_data(**kwargs)
-#         context['post'] = Post.objects.all()
-#         return context
+# #     def get_context_data(self, **kwargs):
+# #         # Call the base implementation first to get a context
+# #         context = super().get_context_data(**kwargs)
+# #         context['post'] = Post.objects.all()
+# #         return context
 
 
-class IndexRedirect(RedirectView):
-    url = 'blog:redirect'
+# # class IndexRedirect(RedirectView):
+# #     url = 'blog:redirect'
 
-    def get_redirect_url(self, *args, **kwargs):
-        return super().get_redirect_url(*args, **kwargs)
+# #     def get_redirect_url(self, *args, **kwargs):
+# #         return super().get_redirect_url(*args, **kwargs)
+
+
+class PostView(ListView):
+    template_name = 'blog/index.html'
+    queryset = Post.objects.filter(status=True)
+    paginate_by = 1
+    ordering = '-id'  
+    context_object_name = 'posts'
+    
+
+    
+
