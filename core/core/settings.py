4321a543357007e941ddc769825783cb0887bcde
Mohammad222PR <mohammades13851@gmail.com>
2023-11-18 03:01:39 +0330

    c

--- core/core/settings.py
+++ core/core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "accounts.apps.AccountsConfig",
     "blog.apps.BlogConfig",
     # Install Package.
+    'django_filters',
     "rest_framework",
     "drf_spectacular",
 ]
