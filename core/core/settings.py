91193269faaaadd0b1166cf49563fca02eaf4aa5
Mohammad222PR <mohammades13851@gmail.com>
2023-11-23 00:42:07 +0330

    c

--- core/core/settings.py
+++ core/core/settings.py
@@ -143,11 +143,11 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": 'rest_framework.schemas.coreapi.AutoSchema',
     
 
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 3,
+    # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    # "PAGE_SIZE": 3,
     # "DEFAULT_PERMISSION_CLASSES": [
     #     "rest_framework.permissions.IsAuthenticated",
-    # ],
+    # # ],
 }
 
 SPECTACULAR_SETTINGS = {
