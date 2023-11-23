d0124d14852264c9514082440d4cc902ea2eb337
Mohammad222PR <mohammades13851@gmail.com>
2023-11-24 02:03:17 +0330

    d

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,10 +1,16 @@
 from rest_framework import generics
 from .serialiezers import Registerationserializer
+from rest_framework.response import Response
+
 
 class RegistrationAPIView(generics.GenericAPIView):
     serializer_class = Registerationserializer
 
     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class()
-        
-        return 
+        serializer = Registerationserializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            date = {
+                'email':''
+            }
+            return Response()
