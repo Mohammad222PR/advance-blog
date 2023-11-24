71c9b084f5092b93bfa287c3e8616b5423e91093
Mohammad222PR <mohammades13851@gmail.com>
2023-11-25 00:39:05 +0330

    c

--- core/accounts/api/v1/serialiezers.py
+++ core/accounts/api/v1/serialiezers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
 from accounts.models import User
 
+
 class Registerationserializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+
+    """
+    for registration user.
+    """
+
     class Meta:
         model = User
-        fields = ['email','password']
+        fields = ["email", "password", "password1"]
+
+    def validate(self, attrs):
+        if attrs.get("password") != attrs.get("password1"):
+            raise serializers.ValidationError(
+                {"detail": "password dose not math try again!"}
+            )
+        else:
+            return super().validate(attrs)
+
+    def create(self, validated_data):
+        
+        return super().create(validated_data)
