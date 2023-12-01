78042214891ff1dec4a08b1430d52f8aa2510e34
Mohammad222PR <mohammades13851@gmail.com>
2023-12-01 03:32:00 +0330

    c

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -9,15 +9,18 @@ from rest_framework_simplejwt.views import (
 app_name = "api-v1"
 
 urlpatterns = [
-    # Default
+    # Default:
     path("registration", views.RegistrationAPIView.as_view(), name="register-api-view"),
-    # Token
+    # Token:
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
     path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
-    # JWT
+    # JWT:
     path("jwt/create/", views.CustomTokenObtainPairView.as_view(), name="jwt-create"),
     path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
     path("jwt/verify/", TokenRefreshView.as_view(), name="jwt-verify"),
-    #Reset Password
+    # Change Password:
     path('password-change/', views.ChangePasswordApiView.as_view(), name="password-change"),
+    # Profile:
+    path('profile/', views.ProfileApiView.as_view(), name='profile')
+
 ]
