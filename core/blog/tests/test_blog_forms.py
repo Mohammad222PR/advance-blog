12feccb77aa10d8f9643d0c3ac7bd074663ee6fe
Mohammad222PR <mohammades13851@gmail.com>
2023-12-11 21:47:37 +0330

    c

--- core/blog/tests/test_blog_forms.py
+++ core/blog/tests/test_blog_forms.py
@@ -1,23 +1,26 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from ..forms import PostCreateForm
+from ..models import Category, Tag
 from datetime import datetime
+
 # Create your tests here.
 
 
 class TestFormPost(TestCase):
+
     def test_post_form_which_valid_data(self):
+        category_obj = Category.objects.create(title='test category')
+        tag_obj = Tag.objects.create(title='test tag')
+
         form = PostCreateForm(
             data={
                 "title": "Test Title",
-                "image": "",
+                "image": None,
                 "content": "Test content",
-                "category": '1',
-                "tags":'1',
-                "created_date":datetime.now(),
-                "updated_date":datetime.now(),
-                "published_day":datetime.now()
+                "category": category_obj,
+                "tag": tag_obj,
+                "created_date": datetime.now(),
             }
         )
         self.assertTrue(form.is_valid())
-        
