1e1d68f90f76d84b81d57a731e608f20fc232a2d
Mohammad222PR <mohammades13851@gmail.com>
2023-12-16 22:27:03 +0330

    x

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,12 +1,24 @@
 import pytest
 from rest_framework.test import APIClient
 from django.urls import reverse, resolve
+from datetime import datetime
 
 @pytest.mark.django_db
-class TestPostApi():
+class TestPostApi:
+    client = APIClient()
 
     def test_get_post_response_200_status(self):
-        client = APIClient()     
-        url = reverse('blog:api-v1:post-list')
-        response = client.get(url)
+        url = reverse("blog:api-v1:post-list")
+        response = self.client.get(url)
         assert response.status_code == 200
+
+    def test_create_post_response_200_status(self):
+        url = reverse("blog:api-v1:post-list")
+        data = {
+            "title": "Test Title",  # input title
+            "content": "Test content",  # input content
+            "created_date": datetime.now(),  # input created
+        }
+        self.client.force_authenticate(user={})
+        response = self.client.post(url, data)
+        assert response.status_code == 403
