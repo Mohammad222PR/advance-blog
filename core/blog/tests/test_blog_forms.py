298ba3223a5b7d4eed4a7409f6b4b23ee4704957
Mohammad222PR <mohammades13851@gmail.com>
2023-12-17 12:02:44 +0330

    c

--- core/blog/tests/test_blog_forms.py
+++ core/blog/tests/test_blog_forms.py
@@ -1,38 +1,38 @@
-# from django.test import TestCase
-# from django.urls import reverse, resolve
-# from ..forms import PostCreateForm
-# from ..models import Category, Tag
-# from datetime import datetime
+from django.test import TestCase
+from django.urls import reverse, resolve
+from ..forms import PostCreateForm
+from ..models import Category, Tag
+from datetime import datetime
 
-# # Create your tests here.
+# Create your tests here.
 
-# # test form models
+# test form models
 
 
-# class TestFormPost(TestCase):
-#     # testing form (PostCreateForm)
+class TestFormPost(TestCase):
+    # testing form (PostCreateForm)
 
-#     def test_post_form_which_valid_data(self):
-#         category_obj = Category.objects.create(title="test category")
-#         tag_obj = Tag.objects.create(title="test tag")
+    def test_post_form_which_valid_data(self):
+        category_obj = Category.objects.create(title="test category")
+        tag_obj = Tag.objects.create(title="test tag")
 
-#         form = PostCreateForm(
-#             data={
-#                 "title": "Test Title",  # input title
-#                 "image": None,  # input image
-#                 "content": "Test content",  # input content
-#                 "category": category_obj,  # input category
-#                 "tag": [tag_obj],  # input tag
-#                 "created_date": datetime.now(),  # input created
-#             }
-#         )
+        form = PostCreateForm(
+            data={
+                "title": "Test Title",  # input title
+                "image": None,  # input image
+                "content": "Test content",  # input content
+                "category": category_obj,  # input category
+                "tag": [tag_obj],  # input tag
+                "created_date": datetime.now(),  # input created
+            }
+        )
 
-#         if not form.is_valid():
-#             print(form.errors)
+        if not form.is_valid():
+            print(form.errors)
 
-#         self.assertTrue(form.is_valid())
+        self.assertTrue(form.is_valid())
 
-#     def test_post_form_which_no_data(self):
-#         form = PostCreateForm(data={})
+    def test_post_form_which_no_data(self):
+        form = PostCreateForm(data={})
 
-#         self.assertFalse(form.is_valid())
+        self.assertFalse(form.is_valid())
--- core/blog/tests/test_blog_urls.py
+++ core/blog/tests/test_blog_urls.py
@@ -1,19 +1,19 @@
-# from django.test import TestCase
-# from django.urls import reverse, resolve
-# from ..views import IndexView, PostView, PostDetailView
+from django.test import TestCase
+from django.urls import reverse, resolve
+from ..views import IndexView, PostView, PostDetailView
 
-# # Create your tests here.
+# Create your tests here.
 
 
-# class TestUrl(TestCase):
-#     def test_blog_index_resolve(self):
-#         url = reverse("blog:index")
-#         self.assertEqual(resolve(url).func.view_class, IndexView)
+class TestUrl(TestCase):
+    def test_blog_index_resolve(self):
+        url = reverse("blog:index")
+        self.assertEqual(resolve(url).func.view_class, IndexView)
 
-#     def test_blog_post_list_url_resolve(self):
-#         url = reverse("blog:blog")
-#         self.assertEqual(resolve(url).func.view_class, PostView)
+    def test_blog_post_list_url_resolve(self):
+        url = reverse("blog:blog")
+        self.assertEqual(resolve(url).func.view_class, PostView)
 
-#     def test_blog_post_detail_url_resolve(self):
-#         url = reverse("blog:post-detail", kwargs={"pk": 15})
-#         self.assertEqual(resolve(url).func.view_class, PostDetailView)
+    def test_blog_post_detail_url_resolve(self):
+        url = reverse("blog:post-detail", kwargs={"pk": 15})
+        self.assertEqual(resolve(url).func.view_class, PostDetailView)
