56f958488ac6ac35d3b9c94f1992d4856ec3ea5d
Mohammad222PR <mohammades13851@gmail.com>
2023-12-22 00:57:27 +0330

    c

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -2,7 +2,6 @@ from locust import HttpUser, task, between
 
 
 class QuickstartUser(HttpUser):
-<<<<<<< Updated upstream
     def on_start(self):
         response = self.client.post(
             "/accounts/api/v2/jwt/create/",data={
@@ -11,7 +10,6 @@ class QuickstartUser(HttpUser):
         }).json()
 
         self.client.headers = {"Authorization": f"Bearer {response.get('access', None)}"}
-=======
 
     def on_start(self):
         response = self.client.post(
@@ -22,7 +20,6 @@ class QuickstartUser(HttpUser):
         self.client.headers = {
             "Authorization": f"Bearer {response.get('access', None)}"
         }
->>>>>>> Stashed changes
 
     @task
     def post_list(self):
@@ -31,8 +28,6 @@ class QuickstartUser(HttpUser):
     @task
     def category_list(self):
         self.client.get("/blog/api/v1/post/cat/")
-<<<<<<< Updated upstream
-=======
 
     @task
     def tag_list(self):
@@ -45,4 +40,3 @@ class QuickstartUser(HttpUser):
     @task
     def index_blog(self):
         self.client.get("/blog/index")
->>>>>>> Stashed changes
