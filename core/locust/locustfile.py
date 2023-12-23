02d191bbab7dcc8249734ccf269c9eabfcaa3e91
Mohammad222PR <mohammades13851@gmail.com>
2023-12-23 12:47:52 +0330

    change

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -1,17 +1,17 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task
 
 
 class QuickstartUser(HttpUser):
     def on_start(self):
         response = self.client.post(
-            "/accounts/api/v2/jwt/create/",data={
-            "email": "mmd@admin.com",
-            "password": "123"
-        }).json()
+            "/accounts/api/v2/jwt/create/",
+            data={"email": "mmd@admin.com", "password": "123"},
+        ).json()
 
-        self.client.headers = {"Authorization": f"Bearer {response.get('access', None)}"}
+        self.client.headers = {
+            "Authorization": f"Bearer {response.get('access', None)}"
+        }
 
-  
     @task
     def post_list(self):
         self.client.get("/blog/api/v1/post/post/")
@@ -27,7 +27,3 @@ class QuickstartUser(HttpUser):
     @task
     def index_blog(self):
         self.client.get("/blog/index/")
-
-    @task
-    def index_blog(self):
-        self.client.get("/blog/index")
